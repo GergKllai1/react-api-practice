73e1063cf2f7a5470ae44b7e0c926a6a4e930794
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-09 12:54:11 +0100

    Adds 2 buttons to change the characters and fetch them every time

--- src/Character.jsx
+++ src/Character.jsx
@@ -5,12 +5,14 @@ class Character extends Component {
     constructor() {
         super();
         this.state = {
+            trigger: 1,
+            new : '',
             characters : []
         }
     }
 
     componentDidMount() {
-        axios.get('https://swapi.co/api/people/1')
+        axios.get('https://swapi.co/api/people/' + this.state.trigger)
         .then(response => {
             this.setState({
                 characters: response.data
@@ -18,9 +20,31 @@ class Character extends Component {
         })
     }
 
+    increaseTrigger() {
+        this.new = this.state.trigger + 1
+        this.setState({trigger: this.new})
+        axios.get('https://swapi.co/api/people/' + this.new)
+        .then(response => {
+            this.setState({
+                characters: response.data
+            })
+        })
+    }
+
+    decreaseTrigger() {
+        if(this.trigger > 1){
+            this.setState({trigger: this.state.trigger + 1}) 
+        }
+    }
+
     render() {
         return(
-            <p>{this.state.characters.name}</p>
+            <div>
+                <button onClick={() => this.decreaseTrigger()}>Previous Character</button>
+                <p>{this.state.characters.name}</p>
+                <button onClick={() => this.increaseTrigger()}>Next Character</button>
+            </div>
+            
         )
     }
 }
